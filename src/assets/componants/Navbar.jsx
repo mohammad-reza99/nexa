55d44e4bc3f2766a7e0b51f5643b5603e38eec79
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-09-07 08:20:33 +0200

    feat: add Navbar component with menu icon and update package dependencies

--- src/assets/componants/Navbar.jsx
+++ src/assets/componants/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { IoMenuOutline } from "react-icons/io5";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
+      <IoMenuOutline className="w-5" />
+    </div>
+  );
 };
 
 export default Navbar;
