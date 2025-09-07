11a23d79e5c615ed15dd4b7a5ceb14a95846ba3b
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-09-07 16:37:20 +0200

    feat: add react-scroll dependency and enhance Footer layout with improved structure

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -9,7 +9,7 @@ import {
 
 const Footer = () => {
   return (
-    <div className="w-full mt-24 bg-slate-900 text-gray-300 py-4 px-2">
+    <div className="w-full mt-24 bg-slate-900 text-gray-300 py-4 px-2 overflow-x-hidden">
       <div className=" max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
         <div>
           <h6 className="font-bold uppercase pt-2">Solution</h6>
@@ -60,15 +60,16 @@ const Footer = () => {
             <input
               className="bg-white w-full mr-4 rounded-md mb-4"
               type="email"
+              placeholder="Enter email..."
             />
             <button className="p-2 mb-4">Subscribe</button>
           </form>
         </div>
       </div>
-      <div>
-        <p>2025 Workflow, LLC, All rights reserved</p>
+      <div className="flex flex-col max-w-[1240px] mx-auto px-2 py-4 justify-between sm:flex-row text-center text-gray-500">
+        <p className="py-4">2025 Workflow, LLC, All rights reserved</p>
       </div>
-      <div>
+      <div className="flex justify-between sm:w-[300px] pt-4 text-2xl">
         <FaFacebook />
         <FaInstagram />
         <FaTwitter />
