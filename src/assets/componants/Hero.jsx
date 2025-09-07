3bb464a3abbd3f7f26d2ab5e92c42ded1a3f917d
Mohammad Reza Zaeri <rezazaeri1990@gmail.com>
2025-09-07 11:28:58 +0200

    fix: adjust padding in Hero component for improved layout

--- src/assets/componants/Hero.jsx
+++ src/assets/componants/Hero.jsx
@@ -7,7 +7,7 @@ import bgImg from "../image/cyber-bg.png";
 const Hero = () => {
   return (
     <div className="w-full h-screen bg-zinc-200 flex flex-col">
-      <div className="grid md:grid-cols-2 max-w-[1240px] mx-auto px-4 pt-30">
+      <div className="grid md:grid-cols-2 max-w-[1240px] mx-auto px-4 pt-24 relative">
         <div className="flex flex-col justify-start md:items-start w-full">
           <p className="text-2xl">Unique sequencing & Production</p>
           <h1 className="py-3 text-5xl md:text-7xl font-bold">
@@ -19,6 +19,29 @@ const Hero = () => {
 
         <div className="flex justify-center items-start">
           <img className="w-full max-w-[500px]" src={bgImg} alt="/" />
+        </div>
+
+        <div
+          className="absolute left-1/2 -translate-x-1/2 -bottom-35 z-10 
+                    bg-zinc-200 border border-slate-300 rounded-xl 
+                    shadow-xl text-center w-[90%] md:min-w-[860px] p-6"
+        >
+          <p className="font-medium">Data Services</p>
+          <div className="flex justify-between flex-wrap px-4">
+            <p className="flex px-4 py-2 text-slate-500">
+              <IoCloudUpload className="h-6 text-indigo-600" />, App Security
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <AiOutlineDatabase className="h-6 text-indigo-600" />, Dashboard
+              Design
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <IoServerSharp className="h-6 text-indigo-600" />, Cloud Data
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <HiMiniPaperAirplane className="h-6 text-indigo-600" />, API
+            </p>
+          </div>
         </div>
       </div>
     </div>
